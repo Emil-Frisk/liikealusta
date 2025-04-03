01cbbfb41001aaf03cbecd4b23ded733aee8fff8
JuusoVainio <juuspa.vainio@gmail.com>
2025-04-03 22:31:40 +0300

    Pitch_test.py tiedosto v3

--- src/pitch_test.py
+++ src/pitch_test.py
@@ -1,15 +1,11 @@
 import math
-import numpy as np
 
-Pitch = -3.0
-Roll = 9.0
-MaxRoll = 0.0
-MinRoll = -0.0
+# Saadaan syötteinä Roll ja Pitch -kulmat
+Pitch = -2.5
+Roll = 10.0
 
-#print("Pitch is:", Pitch,)
-#print("Roll is:", Roll)
-#print("MaxRoll is:", MaxRoll)
-#print("MinRoll is:", MinRoll)
+# Tarkistetaan että annettu pitch -kulma on välillä -8 <-> 8
+Pitch = max(-8, min(Pitch, 8))
 
 # Laske MaxRoll pitch -kulman avulla
 MaxRoll = 0.002964 * Pitch**4 + 0.000939 * Pitch**3 - 0.424523 * Pitch**2 - 0.05936 * Pitch + 15.2481
@@ -17,39 +13,21 @@ MaxRoll = 0.002964 * Pitch**4 + 0.000939 * Pitch**3 - 0.424523 * Pitch**2 - 0.05
 # Laske MinRoll MaxRoll -arvon avulla
 MinRoll = -1 * MaxRoll
 
-print("MaxRoll is:", MaxRoll)
-print("MinRoll is:", MinRoll)
-
 # Verrataan Roll -kulmaa MaxRoll ja MinRoll -arvoihin
-if Roll > MaxRoll:
-    Roll = MaxRoll
-elif Roll < MinRoll:
-    Roll = MinRoll
-
-#print("Pitch is:", Pitch,)
-print("Roll is:", Roll)
-#print("MaxRoll is:", MaxRoll)
-#print("MinRoll is:", MinRoll)
-
-
+Roll = max(MinRoll, min(Roll, MaxRoll))
 
 # Valitse käytettävä Roll -lauseke
-
-if Pitch < -2:
+if Roll == 0:
+    Relaatio = 1
+elif Pitch < -2:
     Relaatio = 0.984723 * (1.5144)**Roll
 elif Pitch > 2:
     Relaatio = 0.999843 * (1.08302)**Roll
-else:
+else:    
     Relaatio = 1.0126 * (1.22807)**Roll
 
-#print("Relaatio on:", Relaatio)
-
-
 # Laske keskipituus
-
 Keskipituus = 0.027212 * (Pitch)**2 + 8.73029 * Pitch + 73.9818
-
-#print("Keskipituus on:", Keskipituus)
 
 # Määritä servomoottorien pituudet
 
@@ -59,6 +37,7 @@ VasenServo = (2 * Keskipituus * Relaatio) / (1 + Relaatio)
 # Oikea moottori
 OikeaServo = (2 * Keskipituus) / (1 + Relaatio)
 
+# Tulostetaan arvot millimetreinä ja kierroksina
 print("Vasemman servon pituus:", VasenServo, "mm")
 print("Oikean servon pituus:", OikeaServo, "mm")
 print()
