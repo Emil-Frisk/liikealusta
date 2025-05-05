501e76ff07efccae3c3551b05902c076d8e24fb9
EmilFrisk1 <vichy9876@gmail.com>
2025-05-05 10:54:10 +0300

    lisäsin bit high low helperin

--- src/test.py
+++ src/test.py
@@ -1,4 +1,6 @@
-high_decimal = 1 << 8
+high_decimal = 2560 >> 7
+
+a = 10
 lower_decimal = 15
 result = high_decimal | lower_decimal
 
@@ -6,3 +8,15 @@ a = 20
 # print(moi)
 velocity_left = high_decimal >> 8
 b = 10
+
+def bit_high_low(number, low_bit):
+    bit_mask = (2**low_bit) - 1
+    register_val_high = number >> low_bit
+    register_val_low = number & bit_mask
+    return (register_val_high, register_val_low)
+
+result = bit_high_low(2560, 7)
+result2 = bit_high_low(40, 4)
+
+a = 10
+
