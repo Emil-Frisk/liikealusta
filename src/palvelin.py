499bf0050a87404eaf5854a98e22f2637f048f9b
Olli Kangasniemi <ollijk133@gmail.com>
2025-05-13 09:59:30 +0300

    deew

--- src/palvelin.py
+++ src/palvelin.py
@@ -3,18 +3,18 @@ import asyncio
 from quart import Quart, request, make_response, jsonify
 from ModbusClients import ModbusClients
 import atexit
-from utils.setup_logging import setup_logging
+from setup_logging import setup_logging
 from launch_params import handle_launch_params
 from module_manager import ModuleManager
 import subprocess
 from time import sleep 
-from utils.utils import is_nth_bit_on, IEG_MODE_bitmask_enable, convert_acc_rpm_revs, convert_vel_rpm_revs
+from utils import is_nth_bit_on, IEG_MODE_bitmask_enable, convert_acc_rpm_revs, convert_vel_rpm_revs
 import math
 import sys
 import os
 import time
 
-async def shutdown_server(app):    
+async def shutdown_test(app):    
     """Gracefully shuts down the server."""
     app.logger.info("Shutdown request received. Cleaning up...")
 
@@ -29,6 +29,7 @@ async def shutdown_server(app):
     await asyncio.sleep(5)
 
     await app.clients.reset_motors()
+
 
     # Cleanup Modbus clients
     if hasattr(app, 'clients') and app.clients:
@@ -103,9 +104,7 @@ def convert_to_revs(pfeedback):
 async def init(app):
     try:
         logger = setup_logging("server", "server.log")
-        app.logger = logger
         module_manager = ModuleManager(logger)
-        app.module_manager = module_manager
         config = handle_launch_params()
         clients = ModbusClients(config=config, logger=logger)
 
@@ -114,19 +113,17 @@ async def init(app):
 
         # Connect to both drivers
         connected = await clients.connect() 
-        app.clients = clients
 
         if not connected:  
-            logger.error(f"""ould not form a connection to both motors,
-                          Left motors ips: {config.SERVER_IP_LEFT}, 
-                          Right motors ips: {config.SERVER_IP_RIGHT}, 
-                         shutting down the server """)
-            cleanup(app)
+            sys.exit(1)
 
         app.app_config = config
+        app.logger = logger
         
+        app.module_manager = module_manager
         app.is_process_done = True
         app.fault_poller_pid = fault_poller_pid
+        app.clients = clients
 
         atexit.register(lambda: cleanup(app))
         
@@ -234,8 +231,8 @@ async def create_app():
             await app.clients.client_left.write_register(address=app.app_config.ANALOG_MODBUS_CNTRL, value=position_client_left, slave=app.app_config.SLAVE_ID)
         elif (roll == "+"):
             (position_client_left, position_client_right) = await get_modbuscntrl_val(app.clients, app.app_config)
-            position_client_left = math.floor(position_client_left + (MODBUSCTRL_MAX* 0.20)) 
-            position_client_right = math.floor(position_client_right - (MODBUSCTRL_MAX* 0.20)) 
+            position_client_left = math.floor(position_client_left + (MODBUSCTRL_MAX* 0.10)) 
+            position_client_right = math.floor(position_client_right - (MODBUSCTRL_MAX* 0.10)) 
 
             position_client_left = min(MODBUSCTRL_MAX, position_client_left)
             position_client_right = max(0, position_client_right)
@@ -249,7 +246,7 @@ async def create_app():
     async def shutdown():
         """Shuts down the server when called."""
         app.logger.info("Shutdown request received.")
-        await shutdown_server(app)
+        await shutdown_test(app)
 
     @app.route('/stop', methods=['get'])
     async def stop_motors():
@@ -259,6 +256,10 @@ async def create_app():
                 pass # do something crazy :O
         except Exception as e:
             app.logger.error("Failed to stop motors?") # Mitäs sitten :D
+
+    @app.route('/asd')
+    async def asd():
+        print("terve")
 
     @app.route('/setvalues', methods=['GET'])
     async def calculate_pitch_and_roll():#serverosote/endpoint?nimi=value&nimi2=value2
