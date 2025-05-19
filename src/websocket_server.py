db0cde512cf11df4aa4deecfea0924cfad4cabf4
Olli Kangasniemi <ollijk133@gmail.com>
2025-05-19 13:49:42 +0300

    ws server updated and validations added

--- src/websocket_server.py
+++ src/websocket_server.py
@@ -9,8 +9,8 @@ from services.monitor_service import create_hearthbeat_monitor_tasks
 from services.motor_service import configure_motor
 from utils.setup_logging import setup_logging
 from utils.utils import extract_part
-
-
+from services.websocket_methods import shutdown,stop_motors,calculate_pitch_and_roll,update_input_values,demo_control
+import services.validation_service as validation_service
 
 class CommunicationHub:
     def __init__(self):
@@ -41,14 +41,17 @@ class CommunicationHub:
             self.logger.error(f"Initialization failed: {e}")
 
 
-    def extract_parts(self, message): # example message: "receiver=startup|identity=fault_poller|message=CRITICAL FAULT!|pitch=40.3"
+    def extract_parts(self, message): # example message: "action=STOP|receiver=startup|identity=fault_poller|message=CRITICAL FAULT!|pitch=40.3"
         receiver = extract_part("receiver=", message=message)
         identity = extract_part("identity=", message=message)
         message = extract_part("message=", message=message)
+        action = extract_part("action=", message=message)
         pitch = extract_part("pitch=", message=message)
         roll = extract_part("roll=", message=message)
+        acceleration = extract_part("acc=", message=message)
+        velocity = extract_part("vel", message=message)
 
-        return (identity, receiver, message)
+        return (identity, receiver, message,action,pitch,roll,acceleration,velocity)
     
     async def handle_client(self, wsclient, path=None):
         # Store client metadata
@@ -59,30 +62,49 @@ class CommunicationHub:
         try:
             async for message in wsclient:
                 print(f"Received: {message}", flush=True)
-                (receiver, identity, message) = self.extract_parts(message)
-                if identity:
-                    client_info["identity"] = identity
-                    print(f"Updated identity for {wsclient.remote_address}: {identity}", flush=True)
-                if receiver:
-                    print(f"Receiver: {receiver}", flush=True)
-
-                # "endpoints"
-                if message == "write":
-                    pass
-                if message == "shutdown":
-                    pass
-                if message == "stop":
-                    pass
-                if message == "setvalues":
-                    pass
-                if message == "updatevalues":
-                    pass
+                (receiver, identity, message,action,pitch,roll,acceleration,velocity) = self.extract_parts(message)
+                if not action:
+                    wsclient.send("No action given, example action=<action>")
+                else: 
+                    if identity:
+                        client_info["identity"] = identity
+                        print(f"Updated identity for {wsclient.remote_address}: {identity}", flush=True)
+                    if receiver:
+                        print(f"Receiver: {receiver}", flush=True)
                     
-                # Send response to clients with identity receiver
-                for client, info in self.wsclients.items():
-                    if info["identity"] == receiver:
-                        await client.send(message)
-                
+                    # "endpoints"
+                    if action == "write":
+                        result = validation_service.validate_pitch_and_roll_values(pitch,roll)
+                        if result:
+                            await demo_control(pitch, roll, self)
+                            
+                    if action == "shutdown":
+                        result = await shutdown(self)
+                        
+                    if action == "stop":
+                        result = await stop_motors(self)
+                        
+                    if action == "setvalues":
+                        try:
+                            result = validation_service.validate_pitch_and_roll_values(pitch, roll)
+                            if result:
+                                calculate_pitch_and_roll(pitch,roll)
+                        except ValueError as e:
+                            self.logger.error(f"ValueError: {e}")
+                            print(f"ValueError: {e}")
+                        except Exception as e:
+                            self.logger.error(f"Error while setting values: {e}")
+                            print(f"Error while setting values: {e}")
+
+                    if action == "updatevalues":
+                        result = await update_input_values(self,acceleration,velocity)
+                        
+                    if action == "message":
+                        (result, msg) = validation_service.validate_message(self,receiver,message)
+                        if result:
+                            receiver.send(msg)
+                        else:
+                            wsclient.send(msg)
 
         except websockets.ConnectionClosed as e:
             print(f"Client {wsclient.remote_address} (identity: {client_info['identity']}) disconnected with code {e.code}, reason: {e.reason}", flush=True)
@@ -102,7 +124,7 @@ async def main():
     await hub.start_server()
 
 
-async def shutdown(self):
+async def shutdown_ws_server(self):
     if hasattr(self, "server") and self.server != None:
         try:
             self.logger.info("Closing websocket server...")
