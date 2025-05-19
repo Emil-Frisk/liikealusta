21240de14611534f370d72289b1f7349887dd763
EmilFrisk1 <vichy9876@gmail.com>
2025-05-19 09:30:56 +0300

    Added logger websocket client py

--- src/websocket_client.py
+++ src/websocket_client.py
@@ -12,17 +12,18 @@ class WebsocketClient():
         self.reconnect_interval = reconnect_interval
         self.max_reconnect_attempt = max_reconnect_attempt
         self.reconnect_count  = 0
+        self.logger = logger
     
     async def connect(self):
         try:
             if self.is_running:
-                print("Client is already connected, can't connect again")
+                self.logger.info("Client is already connected, can't connect again")
                 return 
             
             self.socket = await asyncio.wait_for(websockets.connect(self.uri), timeout=10) 
             self.is_running = True
             self.reconnect_count = 0
-            print(f"client connected to server: {self.uri}")
+            self.logger.info(f"client connected to server: {self.uri}")
             self._listen_task = asyncio.create_task(self._listen())
         except TimeoutError:
             await self.handle_connection_failure(f"Connection timed out after 10 seconds")
@@ -31,25 +32,25 @@ class WebsocketClient():
     
     async def handle_connection_failure(self, error_msg):
         """Handle a connection failure by scheduling a reconnect or closing the client."""
-        print(f"Connection failed: {error_msg}")
+        self.logger.error(f"Connection failed: {error_msg}")
         self.reconnect_count += 1
         if self.reconnect_count < self.max_reconnect_attempt:
             await self._schedule_reconnect()
         else:
-            print("Maximum reconnect attempts reached, closing client")
+            self.logger.info("Maximum reconnect attempts reached, closing client")
             await self.close()
     
     async def _listen(self):
         try:
-            print("Creating listening coroutine for client")
+            self.logger.info("Creating listening coroutine for client")
             while self.is_running:
                 response = await self.socket.recv()
                 if self.on_message:
                     self.on_message(response)
         except ConnectionClosed:
-            print("Client disconnected from the server")
+            self.logger.info("Client disconnected from the server")
         except Exception as e:
-            print(e)
+            self.logger.error(f"Exception in the listen coroutine: {e}")
         finally:
             if self.is_running:
                 self.is_running = False
@@ -63,13 +64,13 @@ class WebsocketClient():
     async def send(self, message):
         try:
             if not self.is_running or self.socket == None:
-                print("Client not connected, can't send a mesasge")
+                self.logger.info("Client not connected, can't send a mesasge")
                 return
 
             await self.socket.send(message)
             return True
         except Exception as e:
-            print(e)
+            self.logger.error(f"Error while client was sending a message: {e}")
             return False
 
     async def close(self):
@@ -86,13 +87,13 @@ class WebsocketClient():
             if self.socket:
                 await self.socket.close()
         except Exception as e:
-            print(e)
+            self.logger.error(f"Exception while closing down a client {e}")
         finally:
             self.socket = None
-            print("client socket closed")
+            self.logger.info("client socket closed")
 
 # def on_message(msg):
-#     print(f"Message callback fired: {msg}")
+#     self.logger..f"Message callback fired: {msg}")
 
 # async def main():
 #     client = WebsocketClient(on_message=on_message)
@@ -100,10 +101,10 @@ class WebsocketClient():
 #     try:
         
 #         while True:
-#             print("Hello from the main function")
+#             self.logger.."Hello from the main function")
 #             await asyncio.sleep(5)
 #     except KeyboardInterrupt:
-#         print("shutting down the client")
+#         self.logger.."shutting down the client")
 #     finally:
 #         await client.close()
 
