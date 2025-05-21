2961b3fe913e09fd6e2e814f7ebf0a575784de1b
Motion Platform <148777334+eetmie@users.noreply.github.com>
2025-05-21 15:30:19 +0300

    asd

--- src/services/gui_service.py
+++ src/services/gui_service.py
@@ -317,7 +317,7 @@ def handle_client_message(self, message):
         self.logger.error(message)
     elif event == "fault":
         self.logger.warning("Fault event has arrived to GUI!")
-        QMessageBox.warning(self, extract_part("message=", message))
+        QMessageBox.warning(self, clientmessage)
         ### TODO - show notification and update fault tab data
     elif event == "faultcleared":
         pass
