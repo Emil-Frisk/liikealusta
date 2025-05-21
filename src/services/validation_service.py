24da8c2a07374fdbc9e7343b6580bc70f50f4d28
EmilFrisk1 <vichy9876@gmail.com>
2025-05-21 10:13:52 +0300

    fixed a bug in validate mssage

--- src/services/validation_service.py
+++ src/services/validation_service.py
@@ -19,8 +19,12 @@ def validate_pitch_and_roll_values(pitch,roll):
 async def validate_message(self,receiver, message):
     if not message:
         return (False, "Action message given, but no actual message found, example: message=<message>")
+    
+    if not receiver:
+        return (False, "No receiver given in the message, example: receiver=<receiver>")
+
     for client, info in self.wsclients.items():
         if info["identity"] == receiver:
             return (True, client, message)
-        return (False, "No receiver given in the message, example: receiver=<receiver>")
+        return (False, "No receiver was found in the server with this identity")
 
