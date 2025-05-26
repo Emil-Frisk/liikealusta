3558c320e59252dd6109c5f7c1eb72cf16367cdb
Motion Platform <148777334+eetmie@users.noreply.github.com>
2025-05-26 12:43:21 +0300

    ?

--- src/helpers/register_val_helpers.py
+++ src/helpers/register_val_helpers.py
@@ -4,6 +4,8 @@ def get_register_values(data):
     right_vals = []
     for register in left_data.registers():
         register
+        b = 10
 
     for register in right_data.registers():
         register
+        c=20
