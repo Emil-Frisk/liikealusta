0255979fc3ba21ec2126c753ccadc2e8114fc070
Motion Platform <148777334+eetmie@users.noreply.github.com>
2025-05-26 12:46:43 +0300

    Finished get register values helper function

--- src/helpers/register_val_helpers.py
+++ src/helpers/register_val_helpers.py
@@ -2,10 +2,11 @@ def get_register_values(data):
     left_data, right_data = data
     left_vals = []
     right_vals = []
-    for register in left_data.registers():
-        register
-        b = 10
+    for register in left_data.registers:
+        left_vals.append(register)
 
-    for register in right_data.registers():
-        register
-        c=20
+    for register in right_data.registers:
+        right_data.append(register)
+        
+    return (left_vals, right_vals)
+    
